ec0b9ea6e44f26d0bde8866966d6018ba0fbd659
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-03-18 12:31:23 +0530

    added all the file with image:

---
+++ CRUD WITH REPO NOT DBS And Tag Helper Img/WebApplication26/WebApplication26/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication26.Models;
+using WebApplication26.Respository;
+namespace WebApplication26.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        private readonly Istudent _istudent;
+
+        public HomeController(Istudent istudent)
+        {
+            _istudent = istudent;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View(_istudent.getStudent().ToList());
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [Route("/yasir")]
+
+        public IActionResult Taghelper()
+        {
+            return View("~/Views/Home/Taghelper.cshtml");
+        }
+
+
+
+        public IActionResult Img()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public IActionResult Create(student s)
+        {
+
+            _istudent.addStudent(s);
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
